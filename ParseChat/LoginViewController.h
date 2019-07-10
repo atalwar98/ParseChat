35baaf23c59fb7896a043d93439d13d238c61846
Anika Talwar <atalwar98@fb.com>
2019-07-10 10:53:15 -0700

    adding login view

---
+++ ParseChat/LoginViewController.h
@@ -0,0 +1,17 @@
+//
+//  LoginViewController.h
+//  ParseChat
+//
+//  Created by atalwar98 on 7/10/19.
+//  Copyright © 2019 atalwar98. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LoginViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ParseChat/LoginViewController.m
@@ -0,0 +1,41 @@
+//
+//  LoginViewController.m
+//  ParseChat
+//
+//  Created by atalwar98 on 7/10/19.
+//  Copyright © 2019 atalwar98. All rights reserved.
+//
+
+#import "LoginViewController.h"
+
+@interface LoginViewController ()
+
+@property (weak, nonatomic) IBOutlet UITextField *parseUsername;
+@property (weak, nonatomic) IBOutlet UITextField *parsePassword;
+
+@end
+
+@implementation LoginViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+- (IBAction)tapSignup:(UIButton *)sender {
+    
+}
+- (IBAction)tapLogin:(UIButton *)sender {
+    
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
